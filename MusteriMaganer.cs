41a4f084842a85d973d8ddd940fdfb0ce1774caf
ouzAk <ouzAk@OGUZ>
2022-05-17 01:46:49 +0300

    Add project files.

---
+++ MusteriMaganer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class CustomerManager
+    {
+        public void AddCustomer(Customer customer)
+        {
+            Console.WriteLine("New Customer Added : Name: " + customer.Name + " id: " + customer.Id + " age :" + customer.Surname);
+        }
+        public void DeleteCustomer(Customer customer)
+        {
+            Console.WriteLine("Customer Deleted : Name: " + customer.Name + " id: " + customer.Id + " age :" + customer.Surname);
+
+        }
+        public void ListCustomers(Customer[] customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("Listing Customer : " + "Name : " + customer.Name + " Id :" + customer.Id + " Age : " + customer.Surname);
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ClassMetotDemo
+
+{
+
+  internal  class MusteriPortfoy
+
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            CustomerManager customerManager = new CustomerManager();
+            Customer[] customers = new Customer[]
+                 {
+                new Customer() { Name = "Oguzcan", Surname ="Akyuz", Id = 36},
+                new Customer() { Name = "Ahmet", Surname = "Kral", Id = 34 },
+                new Customer() { Name = "Mehmet", Surname ="Pala", Id = 23 }
+            };
+                 customerManager.AddCustomer(customers[0]);
+            customerManager.DeleteCustomer(customers[1]);
+
+            Customer customer1 = new Customer() { Name = "Oguzcan", Id = 36, Surname="Akyuz" };
+            Customer customer2 = new Customer() { Name = "Ahmet", Id = 24, Surname="Kral" };
+            Customer customer3 = new Customer() { Name = "Mehmet", Id = 23, Surname="Pala" };
+            Customer[] customers2 = new Customer[] { customer1, customer2, customer3 };
+
+            customerManager.AddCustomer(customer1);
+            Console.WriteLine("*******************");
+
+            customerManager.DeleteCustomer(customer2);
+            Console.WriteLine("*******************");
+
+            customerManager.ListCustomers(customers);
+
+            Console.WriteLine("*******************");
+
+            customerManager.ListCustomers(customers2);
+        }
+
+
+
+        }
+    }
