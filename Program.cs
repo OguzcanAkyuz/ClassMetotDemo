8252ca1d4a00c5c0522fb3852ea22d8ddf20c983
oguzakyuz1 <103968958+oguzakyuz1@users.noreply.github.com>
2022-05-17 01:59:14 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,15 +16,15 @@ namespace ClassMetotDemo
             Customer[] customers = new Customer[]
                  {
                 new Customer() { Name = "Oguzcan", Surname ="Akyuz", Id = 36},
-                new Customer() { Name = "Ahmet", Surname = "Kral", Id = 34 },
-                new Customer() { Name = "Mehmet", Surname ="Pala", Id = 23 }
+                new Customer() { Name = "Ahmet", Surname = "Teker", Id = 34 },
+                new Customer() { Name = "Mehmet", Surname ="Capraz", Id = 23 }
             };
                  customerManager.AddCustomer(customers[0]);
             customerManager.DeleteCustomer(customers[1]);
 
             Customer customer1 = new Customer() { Name = "Oguzcan", Id = 36, Surname="Akyuz" };
-            Customer customer2 = new Customer() { Name = "Ahmet", Id = 24, Surname="Kral" };
-            Customer customer3 = new Customer() { Name = "Mehmet", Id = 23, Surname="Pala" };
+            Customer customer2 = new Customer() { Name = "Ahmet", Id = 24, Surname="Teker" };
+            Customer customer3 = new Customer() { Name = "Mehmet", Id = 23, Surname="Capraz" };
             Customer[] customers2 = new Customer[] { customer1, customer2, customer3 };
 
             customerManager.AddCustomer(customer1);
